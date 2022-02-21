f49f97c93dd805c6066c3a44b99f1fbd58236a73
Gavin <oiamo123@gmail.com>
2022-02-21 16:18:43 -0700

    Add README.me Trying_Things_Out.py hello_world.txt

--- Trying_Things_Out.py
+++ Trying_Things_Out.py
@@ -1,11 +1,23 @@
+import random
 
 def game():
-    secret_word = "Giraffe"
+    words = ['Giraffe', 'Panda', 'Lion', 'Snake', 'Penguin',]
+    secret_word = (random.choice(words))
+    if secret_word == "Giraffe":
+        hint = ("This animal has a long neck")
+    if secret_word == "Panda":
+        hint = ("This animal is black and white is popular in China")
+    if secret_word == "Lion":
+        hint = ("Typically reffered to as king of the jungle")
+    if secret_word == "Snake":
+        hint = ("This animal is long and has no legs")
+    if secret_word == 'Penguin':
+        hint = ("This animal lives in Antartica")
     guess = ""
     count = 0
     max_attempts = 3
     game = True
-    print("Guess the animal: This animal has a long neck")
+    print(hint)
     while guess != secret_word and game:
         if count < max_attempts:
             guess = input("Enter a word: ")
@@ -18,10 +30,14 @@ def game():
             print("You win!")
             main()
 def main():
-    play_again = input("Would you like to play?: ")
-    if play_again == "yes":
-        game()
-    else:
-        print("Good-bye!")
+    try:
+        play_again = input("Would you like to play? Enter 'y' to play again or 'n' to quit: ")
+        if play_again == "y":
+            game()
+        if play_again == "n":
+            print("Good-bye!")
+    except:
+        print("Incorrect response")
+        play_again
 
 main()
